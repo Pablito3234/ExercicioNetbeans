87ed0cd72f94ddc36ffdb3e510bb885b6bb44328
Pablo <182400306@senacinformatica.br>
2024-06-17 21:15:59 -0300

    revisao

--- src/main/java/test1/Main.java
+++ src/main/java/test1/Main.java
@@ -1,8 +1,10 @@
 package test1;
 
 import java.lang.Math;
+
 public class Main {
-    public boolean primo(int numero){
+
+    public boolean primo(int numero) {
         int Divisor = 0;
         for (int i = 1; i <= numero; i++) {
             if (numero % i == 0) {
@@ -15,32 +17,26 @@ public class Main {
             return false;
         }
     }
-    
-    public int sortear(int min, int max){
+
+    public int sortear(int min, int max) {
         int intervalo = (max - min) + 1;
-        return (int) (Math.random() * intervalo) + min; 
-        }
+        return (int) (Math.random() * intervalo) + min;
+    }
 
     public static void main(String[] args) {
 
         Main objeto = new Main();
         int[] vet = new int[20];
-      //  System.out.println(objeto.sortear(10,20));
-        int cont =0;
         for (int i = 0; i < 20; i++) {
-            vet[i]=objeto.sortear(30, 100);
-         System.out.println(vet[i] +"   "+ i );
-      
+            vet[i] = objeto.sortear(30, 100);
+            System.out.println(vet[i] + "   " + i);
         }
+        System.out.println("\n");
 
-        
-       /* System.out.println("\n");
-        
         for (int i = 0; i < 20; i++) {
             if (objeto.primo(vet[i]) == true) {
                 System.out.println(vet[i] + " é primo");
             }
-        }*/   
+        }
     }
-    
 }
